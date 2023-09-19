6a298d1811619f84e1ec86c95644b8f0426f04cc
UhmTex-dev <texroblox@gmail.com>
2023-09-19 21:52:13 +0300

    Updated UI and functionality

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject ThemeSelectPanel;
+    public GameObject MainMenuPanel;
+
+    public void ThemeSelecter()
+    {
+        MainMenuPanel.SetActive(false);
+        ThemeSelectPanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
